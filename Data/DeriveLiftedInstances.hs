705ffc3c343e07a33279d969500165d281f6c74b
Sjoerd Visscher <sjoerd@w3future.com>
2018-09-30 19:25:10 +0200

    ShowsPrec improvements

--- Data/DeriveLiftedInstances.hs
+++ Data/DeriveLiftedInstances.hs
@@ -1,5 +1,4 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE TemplateHaskell, DeriveLift, StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.DeriveLiftedInstances
@@ -12,13 +11,11 @@
 module Data.DeriveLiftedInstances (
   deriveInstance,
   Derivator(..),
+  noopDeriv,
   apDeriv, tupleDeriv, showDeriv, ShowsPrec(..)
 ) where
   
 import Language.Haskell.TH
-import Language.Haskell.TH.Syntax (Lift)
-import Data.Maybe (fromMaybe)
-import Data.Char (isAlpha)
 import Data.DeriveLiftedInstances.Internal
 
 
@@ -38,40 +35,8 @@ tupleDeriv = noopDeriv {
   over = \vv -> [|(fmap fst $vv, fmap snd $vv)|]
 }
 
-deriving instance Lift Fixity
-deriving instance Lift FixityDirection
-data ShowsPrec = ShowsPrec (Int -> String -> String) | ShowOp2 Fixity (Int -> String -> String) | ShowOp1 Fixity (Int -> String -> String)
-instance Show ShowsPrec where
-  showsPrec d (ShowsPrec f) = f d
-  showsPrec d (ShowOp2 (Fixity p _) f) = showParen (d > p) $ f 0
-  showsPrec _ (ShowOp1 _ f) = showParen True $ f 0
-
-showAp :: ShowsPrec -> ShowsPrec -> ShowsPrec
-showAp (ShowsPrec f) (ShowsPrec g) = ShowsPrec $ \d -> showParen (d > 10) $ f 11 . showChar ' ' . g 11
-showAp (ShowOp2 fx@(Fixity p i) f) (ShowsPrec g) = ShowOp1 fx $ \_ -> g (p + fromEnum (i /= InfixL)) . showChar ' ' . f 0
-showAp (ShowOp1 (Fixity p i) f) (ShowsPrec g) = ShowsPrec $ \d -> showParen (d > p) (f 0 . showChar ' ' . g (p + fromEnum (i /= InfixR)))
-showAp _ _ = error "Unexpected use of showAp"
-
-showDeriv :: Derivator
-showDeriv = noopDeriv {
-  op = \nm -> let name = nameBase nm in if isOperator name 
-    then do
-      fx <- fromMaybe defaultFixity <$> reifyFixity nm
-      [|ShowOp2 fx $ const $ showString $(return . LitE . StringL $ name)|]
-    else [|ShowsPrec $ const $ showString $(return . LitE . StringL $ name)|],
-  ap = \f a -> [|showAp $f $a|],
-  arg = \t v -> do
-    test <- isClassInstance ''Show t 
-    if test then [|ShowsPrec $ flip showsPrec $v|] else [|ShowsPrec $ const (showString "#Unshowable#") |],
-  var = \v -> [|ShowsPrec $ flip showsPrec $v|],
-  over = \v -> [|ShowsPrec $ flip showsPrec $v|]
-} 
-
-isClassInstance :: Name -> Type -> Q Bool
-isClassInstance nm t = do
-  ClassI _ insts <- reify nm
-  return $ any (== t) [ t' | InstanceD _ _ (AppT _ t') _ <- insts]
-
-isOperator :: String -> Bool
-isOperator (c:_) = not (isAlpha c) && c /= '_'
-isOperator _ = False
+deriveInstance showDeriv [t| Num ShowsPrec |]
+deriveInstance showDeriv [t| Fractional ShowsPrec |]
+deriveInstance showDeriv [t| Floating ShowsPrec |]
+deriveInstance showDeriv [t| Semigroup ShowsPrec |]
+deriveInstance showDeriv [t| Monoid ShowsPrec |]
--- Data/DeriveLiftedInstances/Internal.hs
+++ Data/DeriveLiftedInstances/Internal.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TemplateHaskell, RecordWildCards #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE TemplateHaskell, RecordWildCards, DeriveLift, StandaloneDeriving #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.DeriveLiftedInstances.Internal
@@ -11,8 +12,10 @@
 module Data.DeriveLiftedInstances.Internal where
   
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax (Lift)
+import Data.Char (isAlpha)
+import Data.Maybe (fromMaybe, catMaybes)
 import Data.Traversable (for)
-import Data.Maybe (catMaybes)
 
 
 data Derivator = Derivator {
@@ -92,3 +95,42 @@ isVar _ _ = False
 tvName :: TyVarBndr -> Name
 tvName (PlainTV nm) = nm
 tvName (KindedTV nm _) = nm
+
+
+deriving instance Lift Fixity
+deriving instance Lift FixityDirection
+data ShowsPrec = ShowsPrec (Int -> String -> String) | ShowOp2 Fixity (Int -> String -> String) | ShowOp1 Fixity (Int -> String -> String)
+instance Show ShowsPrec where
+  showsPrec d (ShowsPrec f) = f d
+  showsPrec d (ShowOp2 (Fixity p _) f) = showParen (d > p) $ f 0
+  showsPrec _ (ShowOp1 _ f) = showParen True $ f 0
+
+showAp :: ShowsPrec -> ShowsPrec -> ShowsPrec
+showAp (ShowsPrec f) (ShowsPrec g) = ShowsPrec $ \d -> showParen (d > 10) $ f 10 . showChar ' ' . g 11
+showAp (ShowOp2 fx@(Fixity p i) f) (ShowsPrec g) = ShowOp1 fx $ \_ -> g (p + fromEnum (i /= InfixL)) . showChar ' ' . f 0
+showAp (ShowOp1 (Fixity p i) f) (ShowsPrec g) = ShowsPrec $ \d -> showParen (d > p) (f 0 . showChar ' ' . g (p + fromEnum (i /= InfixR)))
+showAp _ _ = error "Unexpected use of showAp"
+
+showDeriv :: Derivator
+showDeriv = noopDeriv {
+  op = \nm -> let name = nameBase nm in if isOperator name 
+    then do
+      fx <- fromMaybe defaultFixity <$> reifyFixity nm
+      [|ShowOp2 fx $ const $ showString $(return . LitE . StringL $ name)|]
+    else [|ShowsPrec $ const $ showString $(return . LitE . StringL $ name)|],
+  ap = \f a -> [|showAp $f $a|],
+  arg = \t v -> do
+    test <- isClassInstance ''Show t 
+    if test then [|ShowsPrec $ flip showsPrec $v|] else [|ShowsPrec $ const (showString "#Unshowable#") |],
+  var = \v -> [|ShowsPrec $ flip showsPrec $v|],
+  over = \v -> [|ShowsPrec $ flip showsPrec $v|]
+} 
+
+isClassInstance :: Name -> Type -> Q Bool
+isClassInstance nm t = do
+  ClassI _ insts <- reify nm
+  return $ any (== t) [ t' | InstanceD _ _ (AppT _ t') _ <- insts]
+
+isOperator :: String -> Bool
+isOperator (c:_) = not (isAlpha c) && c /= '_'
+isOperator _ = False
